4ba7cde560a413ec315cf072a8042a92580cfc4c
GeovanniMercz <geza.mercz@cceo.com.mx>
2025-04-09 17:11:34 -0600

    feat(): Implementing Active Classes for Home and Pokémon Buttons

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,15 @@ import HelloWorld from "./components/HelloWorld.vue";
         <img
           src="@/assets/logo.svg"
           alt="Logo"
-          width="30"
+          width="50"
           height="24"
           class="d-inline-block align-text-top"
         />
         PokeApi
       </router-link>
       <div>
-        <router-link class="btn btn-outline-primary" to="/"> Home </router-link>
-        <router-link class="btn btn-outline-primary" to="/pokemons"> Pokemons </router-link>
+        <router-link class="btn btn-outline-primary  me-3" to="/"> Home </router-link>
+        <router-link class="btn btn-outline-primary " to="/pokemons"> Pokemons </router-link>
       </div>
     </div>
   </nav>
