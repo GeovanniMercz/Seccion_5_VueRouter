59c9b2731eae3dbbf6d090013f82405e6bbf5056
GeovanniMercz <geza.mercz@cceo.com.mx>
2025-04-09 17:18:44 -0600

    feat():Deploy the 404 screen if you can't find the specified path or a specific item

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+
+
+</script>
+
+<template>
+    <h1>
+        404
+    </h1>
+    <router-link class="btn btn-outline-primary  me-3" to="/"> Go Home </router-link>
+</template>
