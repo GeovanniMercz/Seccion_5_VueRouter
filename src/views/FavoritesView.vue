e54b9c04d14dc5cbbc1e4cde19fb37e7c11baeed
GeovanniMercz <geza.mercz@cceo.com.mx>
2025-04-10 16:41:26 -0600

    Fix():Bug fix in Duplicate Item Favorites view

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -7,22 +7,18 @@ const { favoritos } = storeToRefs(useFavoritos);
 </script>
 
 <template>
-    <h1>Favoritos</h1>
-    <ul class="list-group">
-        <li
-  v-for="poke in favoritos.filter(p => p?.id && p?.name && p?.sprite)"
-  :key="poke.id"
-  class="list-group-item d-flex align-items-center gap-3"
->
-  <img :src="poke.sprite" alt="Imagen de Pokémon" width="50" />
-  <div>
-    <p><strong>ID:</strong> {{ poke.id }}</p>
-    <p><strong>Nombre:</strong> {{ poke.name }}</p>
-  </div>
-</li>
-
-    </ul>
-  </template>
-  
-
-  
+  <h1>Favoritos</h1>
+  <ul class="list-group">
+    <li
+      v-for="poke in favoritos.filter((p) => p?.id && p?.name && p?.sprite)"
+      :key="poke.id"
+      class="list-group-item d-flex align-items-center gap-3"
+    >
+      <img :src="poke.sprite" alt="Imagen de Pokémon" width="50" />
+      <div>
+        <p><strong>ID:</strong> {{ poke.id }}</p>
+        <p><strong>Nombre:</strong> {{ poke.name }}</p>
+      </div>
+    </li>
+  </ul>
+</template>
