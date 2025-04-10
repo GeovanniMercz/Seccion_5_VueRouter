965c88dee296ae16a9bde2c3bd6e5682c666a390
GeovanniMercz <geza.mercz@cceo.com.mx>
2025-04-10 16:40:42 -0600

    feat():Add view from Favorite elements with ID, name and Image of each item

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,8 +1,28 @@
-<script>
+<script setup>
+import { useFavoritoStore } from "@/store/favoritos";
+import { storeToRefs } from "pinia";
+
+const useFavoritos = useFavoritoStore();
+const { favoritos } = storeToRefs(useFavoritos);
 </script>
 
 <template>
-    <H1>
-        Favoritos
-    </H1>
-</template>
+    <h1>Favoritos</h1>
+    <ul class="list-group">
+        <li
+  v-for="poke in favoritos.filter(p => p?.id && p?.name && p?.sprite)"
+  :key="poke.id"
+  class="list-group-item d-flex align-items-center gap-3"
+>
+  <img :src="poke.sprite" alt="Imagen de Pokémon" width="50" />
+  <div>
+    <p><strong>ID:</strong> {{ poke.id }}</p>
+    <p><strong>Nombre:</strong> {{ poke.name }}</p>
+  </div>
+</li>
+
+    </ul>
+  </template>
+  
+
+  
--- src/views/PokeView.vue
+++ src/views/PokeView.vue
@@ -2,28 +2,45 @@
 import axios from "axios";
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import {useFavoritoStore} from '@/store/favoritos'
 
 const route = useRoute();
 const router = useRouter();
 const pokeSprite = ref({});
+const useFavorito = useFavoritoStore()
+
+const {add} = useFavorito
 
 const back = () => {
     router.push("/pokemons");
 };
 
 const getData = async () => {
-    console.log(route.params.name);
-    try {
-        const { data } = await axios.get(
-            `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
-        );
-        console.log(data)
-        pokeSprite.value = data.sprites.front_default;
-    } catch (error) {
-        console.log(error);
-        pokeSprite.value = null;
-    }
+  console.log(route.params.name);
+  try {
+    const { data } = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
+    );
+    console.log(data);
+
+    // Extraemos las propiedades necesarias
+    const poke = {
+      id: data.id,
+      name: data.name,
+      sprite: data.sprites.front_default,  // Esto lo puedes usar en el template si quieres
+    };
+
+    // Llamamos a add con el objeto correcto
+    add(poke);
+
+    pokeSprite.value = data.sprites.front_default;
+  } catch (error) {
+    console.log(error);
+    pokeSprite.value = null;
+  }
 };
+
+
 
 getData();
 </script>
@@ -32,8 +49,8 @@ getData();
     <main class="text-center">
         <div v-if="pokeSprite">
             <img :src="pokeSprite" alt="" />
-           
             <h1>Poke: {{ $route.params.name }}</h1>
+            <button class="btn btn-primary mb-2" @click="add(data)">Agregar Favoritos</button>
         </div>
         <h1 v-else>Pokemon no encontrado...</h1>
         <button @click="back()">Volver al listado</button>
